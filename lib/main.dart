2fa81eeb5ae5108b6d60c341e5e1b30baadac05d
HassamAli2174 <hassamarshad021@gmail.com>
2024-08-17 17:34:41 +0500

    Made inital Splash Screen

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:rest_api_movie_application/Pages/splash_page.dart';
+
+void main() {
+  runApp(
+    SplashPage(key: UniqueKey(), onInitializationComplete: () {}),
+  );
+}
