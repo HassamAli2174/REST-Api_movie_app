9a61c4301527eeb7018f63927fb55c0c63e73bcb
HassamAli2174 <hassamarshad021@gmail.com>
2024-08-20 23:44:07 +0500

    "Added initialization delay to SplashPage, refactored main function to use ProviderScope and introduced new MyApp widget"

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:rest_api_movie_application/Pages/main_page.dart';
 import 'package:rest_api_movie_application/Pages/splash_page.dart';
 
 void main() {
   runApp(
-    SplashPage(key: UniqueKey(), onInitializationComplete: () {}),
+    SplashPage(
+      key: UniqueKey(),
+      onInitializationComplete: () => runApp(
+        const ProviderScope(
+          child: MyApp(),
+        ),
+      ),
+    ),
   );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Movie App',
+      initialRoute: 'home',
+      routes: {
+        'home': (context) => MainPage(),
+      },
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+    );
+  }
 }
